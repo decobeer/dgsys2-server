1a4b03ad53ff1e0057267a82c722044c5c5136f5
jonasgiske <jbgiske@akvagroup.com>
2020-06-15 12:39:13 +0200

    config static files 3

--- dgsys_server/settings.py
+++ dgsys_server/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dgsys_server.urls'
